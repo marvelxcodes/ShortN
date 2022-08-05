952765f5b2188967c9df203ba2a29c9c82b04a24
Rama Krishnan <ramakrishnan0046@gmail.com>
2022-08-05 06:12:10 +0530

    Minor Bug fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,8 +11,10 @@ const Header:FC<HeaderProps> = ({scroll}) => {
 <Head>
   <title>ShortN</title>
   <meta name="description" content="A free URL Shortener by MarvelXCodes" />
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 </Head>
 <header className='h-16 shadow-2xl relative w-full flex items-center px-8 md:px-20'>
+  <img src="/favicon.ico" height={30} className="mr-3" width={30} alt="" />
   <h1 className='text-xl font-bold select-none'>ShortN</h1>
   <div className="absolute right-5 md:right-20 flex items-center">
     <h1 className='font-bold mr-3 select-none hidden md:flex'>Shorten your URL &gt;</h1>
