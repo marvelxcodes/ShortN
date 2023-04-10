7bf9fbecb0b67945c12b8f7bacc2aa3de67d4a09
marvelxcodes <ramakrishnan0046@gmail.com>
2023-04-11 02:29:31 +0530

    Addeď explicit types

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,30 +1,41 @@
-import prisma from "@libs/prisma"
-import { GetServerSidePropsContext } from "next"
-import { useRouter } from "next/router"
-import { useEffect } from "react"
+import prisma from '@libs/prisma';
+import { GetServerSidePropsContext, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-export default function Redirect({url}:any) {
-  const { push } = useRouter()
-  useEffect(() => {
-    if (url) {
-      push(url.url)
-  }})
-  return (
-<div className="flex items-center justify-center h-screen w-screen">
-    <p className="text-6xl text-purple-600 font-extrabold">{url?"Redirecting...":"404 | Page Not Found"}</p>
-</div>
-)}
-
-export const getServerSideProps = async (context:GetServerSidePropsContext) => {
-  const { id } = context.query
-  const url = await prisma.uRL.findUnique({
-    where: {
-      id: String(id)
-    }
-  })
-  return {
-    props: {
-      url
-    }
+export type PageProps = {
+	res: {
+    id: string;
+    url: string;
   }
+};
+
+export default function Redirect({ res }: PageProps) {
+	const { push } = useRouter();
+	useEffect(() => {
+		push(res.url);
+	});
+	return (
+		<div className='flex items-center justify-center h-screen w-screen'>
+			<p className='text-6xl text-purple-600 font-extrabold'>
+				{res.url ? 'Redirecting...' : '404 | Page Not Found'}
+			</p>
+		</div>
+	);
 }
+
+export const getServerSideProps = async (
+	context: GetServerSidePropsContext
+) => {
+	const { id } = context.query;
+	const res = await prisma.uRL.findUnique({
+		where: {
+			id: String(id),
+		},
+	});
+	return {
+		props: {
+			res
+		},
+	};
+};
